252b8b29f5acc1a7e9f603229a292cdaac1727b7
Ana Maria <anamaria452dd@gmail.com>
2025-01-17 13:30:17 -0300

    escrevendo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# teste
-aqui vou fazer o teste inicial
+# projeto1-one-
+Vou colocar aqui o que consegui fazer do 1 curso do ONE(evento da Oracle+Alura)
---
+++ desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,43 @@
+// alert('Boas vindas ao nosso site!');
+
+// let nome = "Lua";
+// let idade = 25;
+// let numeroDeVendas = 50;
+// let saldoDisponivel = 1000;
+
+
+// alert('Erro! Preencha todos os campos');
+
+// let mensagemDeErro = "Erro! Preencha todos os campos"
+
+// alert(mensagemDeErro);
+
+// let nome1 = prompt('Informe seu nome ');
+// let idade1 = prompt('Informe sua idade');
+
+// if( idade1 >=18){
+//     alert('Pode tirar a habilitaçao');
+// }
+
+let dia = prompt('Qual e o dia da semana?');
+
+if (dia == 'Sábado' || dia == 'Domingo'){
+    alert('Bom fim de semana!');
+} else{
+    alert('Boa semana!');
+}
+
+let number = prompt('Digite um numero');
+
+if (number <0){
+    alert('Esse numero e negativo');
+}else{
+    alert('Esse numero e positivo');
+}
+
+let saldoConta = 500;
+alert(`${saldoConta}`);
+
+let nome = prompt('Informe seu nome');
+
+alert('Boas vindas'+nome);
